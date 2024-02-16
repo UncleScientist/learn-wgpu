32c99bc8142f020b1bc4323e33ab6a3f47f73d64
Uncle Scientist <unclescientist@gmail.com>
2024-02-16 08:13:24 -0500

    changing the vertex colors to help the triangles stand out

--- src/lib.rs
+++ src/lib.rs
@@ -18,23 +18,23 @@ struct Vertex {
 const VERTICES: &[Vertex] = &[
     Vertex {
         position: [-0.0868241, 0.49240386, 0.0],
-        color: [0.5, 0.0, 0.5],
+        color: [0.8, 0.0, 0.5],
     },
     Vertex {
         position: [-0.49513406, 0.06958647, 0.0],
-        color: [0.5, 0.0, 0.5],
+        color: [0.5, 0.4, 0.5],
     },
     Vertex {
         position: [-0.21918549, -0.44939706, 0.0],
-        color: [0.5, 0.0, 0.5],
+        color: [0.5, 0.0, 0.2],
     },
     Vertex {
         position: [0.35966998, -0.3473291, 0.0],
-        color: [0.5, 0.0, 0.5],
+        color: [0.5, 0.7, 0.5],
     },
     Vertex {
         position: [0.44147372, 0.2347359, 0.0],
-        color: [0.5, 0.0, 0.5],
+        color: [0.1, 0.0, 0.5],
     },
 ];
 
