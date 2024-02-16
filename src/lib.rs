6cd5a269cad300c069965cbc0cf9c3309e00b832
Uncle Scientist <unclescientist@gmail.com>
2024-02-16 08:12:06 -0500

    drawing a magenta pentagon

--- src/lib.rs
+++ src/lib.rs
@@ -17,18 +17,28 @@ struct Vertex {
 
 const VERTICES: &[Vertex] = &[
     Vertex {
-        position: [0.0, 0.5, 0.0],
-        color: [1.0, 0.0, 0.0],
+        position: [-0.0868241, 0.49240386, 0.0],
+        color: [0.5, 0.0, 0.5],
     },
     Vertex {
-        position: [-0.5, -0.5, 0.0],
-        color: [0.0, 1.0, 0.0],
+        position: [-0.49513406, 0.06958647, 0.0],
+        color: [0.5, 0.0, 0.5],
     },
     Vertex {
-        position: [0.5, -0.5, 0.0],
-        color: [0.0, 0.0, 1.0],
+        position: [-0.21918549, -0.44939706, 0.0],
+        color: [0.5, 0.0, 0.5],
+    },
+    Vertex {
+        position: [0.35966998, -0.3473291, 0.0],
+        color: [0.5, 0.0, 0.5],
+    },
+    Vertex {
+        position: [0.44147372, 0.2347359, 0.0],
+        color: [0.5, 0.0, 0.5],
     },
 ];
+
+const INDICES: &[u16] = &[0, 1, 4, 1, 2, 4, 2, 3, 4];
 
 impl Vertex {
     fn desc() -> wgpu::VertexBufferLayout<'static> {
@@ -153,7 +163,9 @@ struct State {
     size: winit::dpi::PhysicalSize<u32>,
     render_pipeline: [wgpu::RenderPipeline; 2],
     vertex_buffer: wgpu::Buffer,
-    num_vertices: u32,
+    _num_vertices: u32,
+    index_buffer: wgpu::Buffer,
+    num_indices: u32,
     which: usize,
     // The window must be declared after the surface so
     // it gets dropped after it as the surface contains
@@ -328,6 +340,12 @@ impl State {
             usage: wgpu::BufferUsages::VERTEX,
         });
 
+        let index_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+            label: Some("Index buffer"),
+            contents: bytemuck::cast_slice(INDICES),
+            usage: wgpu::BufferUsages::INDEX,
+        });
+
         Self {
             window,
             surface,
@@ -338,7 +356,9 @@ impl State {
             which: 0,
             render_pipeline: [render_pipeline1, render_pipeline2],
             vertex_buffer,
-            num_vertices: VERTICES.len() as u32,
+            _num_vertices: VERTICES.len() as u32,
+            index_buffer,
+            num_indices: INDICES.len() as u32,
         }
     }
 
@@ -396,7 +416,8 @@ impl State {
 
             render_pass.set_pipeline(&self.render_pipeline[self.which]);
             render_pass.set_vertex_buffer(0, self.vertex_buffer.slice(..));
-            render_pass.draw(0..self.num_vertices, 0..1);
+            render_pass.set_index_buffer(self.index_buffer.slice(..), wgpu::IndexFormat::Uint16);
+            render_pass.draw_indexed(0..self.num_indices, 0, 0..1);
         }
 
         // submit will accept anything that implements IntoIter
